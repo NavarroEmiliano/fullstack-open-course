cfbb6af3f76886f615f48e05693322eb3ddda0ca
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-06-21 02:50:53 -0300

    eslint config

---
+++ fso-part9-typescript-express/eslint.config.mjs
@@ -0,0 +1,26 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  {
+    rules: {
+      "@typescript-eslint/semi": ["error"],
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/restrict-template-expressions": "off",
+      "@typescript-eslint/restrict-plus-operands": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { "argsIgnorePattern": "^_" }
+      ],
+      "no-case-declarations": "off"
+    }
+  },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended
+];
